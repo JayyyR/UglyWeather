02c420363ac095409300faadca97a3deee09b916
unknown <flatpita92@gmail.com>
2017-12-25 21:12:23 -0500

    models

---
+++ app/src/main/java/com/joeracosta/uglyweather/model/NowWeather.kt
@@ -0,0 +1,25 @@
+package com.joeracosta.uglyweather.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by Joe on 12/25/2017.
+ */
+data class NowWeather(
+
+        @SerializedName("icon")
+        val icon : String? = null,
+
+        @SerializedName("temperature")
+        val temperature : Float? = null,
+
+        @SerializedName("apparentTemperature")
+        val feelsLike : Float? = null,
+
+        @SerializedName("humidity")
+        val humidity : Float? = null,
+
+        @SerializedName("summary")
+        val summary : String? = null
+
+)
---
+++ app/src/main/java/com/joeracosta/uglyweather/model/WeatherResponse.kt
@@ -0,0 +1,12 @@
+package com.joeracosta.uglyweather.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by Joe on 12/25/2017.
+ */
+
+data class WeatherResponse(
+        @SerializedName("currently")
+        val nowWeather: NowWeather? = null
+)
